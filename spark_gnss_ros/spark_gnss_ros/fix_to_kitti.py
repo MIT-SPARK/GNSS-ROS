0546789fd2ed1aeb4b8cad45a0bd5f6939ba38fd
Toya Takahashi <toyatakahashi522@gmail.com>
2025-05-13 21:37:38 -0400

    added gps fix to kitti script

---
+++ spark_gnss_ros/spark_gnss_ros/fix_to_kitti.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import sqlite3
+import argparse
+import utm
+from pathlib import Path
+from rclpy.serialization import deserialize_message
+from rosidl_runtime_py.utilities import get_message
+
+
+def extract_navsatfix_from_bag(db3_path: Path, topic_name: str = "/fix"):
+    msg_type = get_message("sensor_msgs/msg/NavSatFix")
+    gps_data = []
+
+    conn = sqlite3.connect(db3_path)
+    cursor = conn.cursor()
+
+    # Get topic ID
+    cursor.execute("SELECT id FROM topics WHERE name=?", (topic_name,))
+    result = cursor.fetchone()
+    if result is None:
+        raise ValueError(f"Topic '{topic_name}' not found in bag.")
+    topic_id = result[0]
+
+    # Fetch messages
+    cursor.execute("SELECT data FROM messages WHERE topic_id=?", (topic_id,))
+    for row in cursor.fetchall():
+        msg = deserialize_message(row[0], msg_type)
+        gps_data.append((msg.latitude, msg.longitude, msg.altitude))
+
+    conn.close()
+    return gps_data
+
+
+def save_as_kitti(gps_data, filename):
+    with open(filename, "w") as f:
+        for lat, lon, z in gps_data:
+            x, y, _, _= utm.from_latlon(lat, lon)
+            f.write(f"1 0 0 {x} 0 1 0 {y} 0 0 1 {z}\n")
+    print(f"Saved KITTI-formatted GPS data to {filename}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert NavSatFix ROS2 bag to KITTI format")
+    parser.add_argument("bag_path", type=Path, help="Path to ROS2 SQLite bag file (.db3)")
+    parser.add_argument("--topic", default="/fix", help="Topic name with GPS data")
+    parser.add_argument("--output", default="fix.txt", type=Path, help="Output txt file")
+    args = parser.parse_args()
+
+    gps_data = extract_navsatfix_from_bag(args.bag_path, args.topic)
+    save_as_kitti(gps_data, args.output)
+
+
+if __name__ == "__main__":
+    main()
